930c390b30bcc1342dcf949736be647d26a534f4
GrowlyX <rtaofficialtechie@Gmail.com>
2021-12-10 21:09:44 -0500

    Some class

---
+++ shared/src/main/kotlin/com/github/thickdog/SomeClass.kt
@@ -0,0 +1,13 @@
+package com.github.thickdog
+
+/**
+ * @author GrowlyX
+ * @since 12/10/2021
+ */
+class SomeClass
+{
+    fun youngLaFlameHeInSickoMode()
+    {
+        println("Young LaFlame, he in sicko mode")
+    }
+}
